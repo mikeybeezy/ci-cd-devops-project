e3a443d169a323d0e145267b80a7845362086ba1
Mike Babs <mikeybabs40@gmail.com>
2022-12-23 15:30:21 +0000

    add loop for resources

--- version.tf
+++ version.tf
@@ -1,18 +1,1 @@
-resource "aws_vpc" "main" {
-  cidr_block       = var.cidr_block
-  instance_tenancy = "default"
 
-  tags = var.tagsq
-}
-
-
-variable "cidr_block " {
-  description = "value"
-  default     = "10.0.0.0/16"
-}
-
-
-variable "tags " {
-  description = "value"
-  default     = "default"
-}
